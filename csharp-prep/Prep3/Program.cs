70dce5aa344dcaf01ed722c6c321699bdcbd5813
adamrash11 <rashfamadam@gmail.com>
2023-01-11 17:28:31 -0700

    I wrote new stuff in prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int specialinteger = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != specialinteger);
+        {
+            Console.Write("What is your guess for the random integer between 1 and 101? ");
+            guess = int.Parse(Console.ReadLine());
+            if (specialinteger > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (specialinteger < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the correct number! ");
+            }
+
+        }
     }
 }
